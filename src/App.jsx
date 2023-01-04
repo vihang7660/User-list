936bb9de77838c1b834ae6a7bb893e9faf103e9d
Vihang Supare <vihang7660@gmail.com>
2023-01-05 00:00:29 +0530

    fetch data and use it to create a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import UserList from "./UserList";
+
 function App() {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
+      .then((resp) => resp.json())
+      .then((data) => setUsers(data.data));
+  }, []);
+
+  console.log(users);
+
+  return (
+    <div>
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/UserList.jsx
@@ -0,0 +1,24 @@
+export default function UserList({users}) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>E-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
